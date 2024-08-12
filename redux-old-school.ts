f8492056bb97f015360fd7cea7a972f37bcd68a3
Wander Amaral <wanderamaral013@gmail.com>
2024-08-12 16:54:08 -0300

    commit

---
+++ redux-old-school.ts
@@ -0,0 +1,24 @@
+import { createStore } from "redux";
+
+const initialState = {
+  counter: {
+    value: 0,
+  },
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "counter/increment": {
+      return {
+        ...state,
+        counter: {
+          value: state.counter.value + 1,
+        },
+      };
+    }
+    default:
+      return state;
+  }
+}
+
+export const store = createStore(reducer);
